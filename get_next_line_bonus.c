237cc80b800ef434e422d3361b219ddbdb76324b
Gauthier Bach <gabach@z2r10p8.42lyon.fr>
2025-11-27 11:28:24 +0100

    finished get_next_line and its bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,18 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gabach <gabach@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 15:18:43 by gabach            #+#    #+#             */
-/*   Updated: 2025/11/27 11:27:50 by gabach           ###   ########lyon.fr   */
+/*   Updated: 2025/11/27 11:27:39 by gabach           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
-static char	*ft_realloc(char *line, int to_add, char buffer[BUFFER_SIZE + 1])
+static char	*ft_realloc(char *line, int to_add, \
+char rests[1025][BUFFER_SIZE + 1], int fd)
 {
 	int		index_l;
 	int		index_b;
@@ -35,12 +36,12 @@ static char	*ft_realloc(char *line, int to_add, char buffer[BUFFER_SIZE + 1])
 		return (NULL);
 	index_b = -1;
 	while (++index_b < to_add)
-		new_line[index_l + index_b] = buffer[index_b];
+		new_line[index_l + index_b] = rests[fd][index_b];
 	new_line[index_l + index_b] = '\0';
 	return (new_line);
 }
 
-static char	*load_line(int fd, char buffer[BUFFER_SIZE + 1], char	*line)
+static char	*load_line(int fd, char rests[1025][BUFFER_SIZE + 1], char	*line)
 {
 	int		index;
 	int		read_result;
@@ -48,16 +49,16 @@ static char	*load_line(int fd, char buffer[BUFFER_SIZE + 1], char	*line)
 	while (1)
 	{
 		index = -1;
-		while (++index < BUFFER_SIZE && buffer[index])
-			if (buffer[index] == '\n')
-				return (ft_realloc(line, index + 1, buffer));
-		if (buffer[0] != '\0')
-			line = ft_realloc(line, index, buffer);
-		if (buffer[0] != '\0' && line == NULL)
+		while (++index < BUFFER_SIZE && rests[fd][index])
+			if (rests[fd][index] == '\n')
+				return (ft_realloc(line, index + 1, rests, fd));
+		if (rests[fd][0] != '\0')
+			line = ft_realloc(line, index, rests, fd);
+		if (rests[fd][0] != '\0' && line == NULL)
 			return (NULL);
-		read_result = read(fd, buffer, BUFFER_SIZE);
+		read_result = read(fd, rests[fd], BUFFER_SIZE);
 		if (read_result == -1 || read_result == 0)
-			buffer[0] = '\0';
+			rests[fd][0] = '\0';
 		if (read_result == -1)
 		{
 			free(line);
@@ -65,46 +66,46 @@ static char	*load_line(int fd, char buffer[BUFFER_SIZE + 1], char	*line)
 		}
 		if (read_result == 0)
 			return (line);
-		buffer[read_result] = '\0';
+		rests[fd][read_result] = '\0';
 	}
 }
 
-static void	save_rest(char buffer[BUFFER_SIZE + 1])
+static void	save_rest(int fd, char rests[1025][BUFFER_SIZE + 1])
 {
 	int	start;
 	int	len;
 	int	index;
 
 	start = 0;
-	while (buffer[start] != '\0' && buffer[start] != '\n')
+	while (rests[fd][start] != '\0' && rests[fd][start] != '\n')
 		start++;
-	if (buffer[start] == '\0')
+	if (rests[fd][start] == '\0')
 	{
-		buffer[0] = '\0';
+		rests[fd][0] = '\0';
 		return ;
 	}
 	start++;
 	len = 0;
-	while (buffer[start + len] && (start + len) < BUFFER_SIZE)
+	while (rests[fd][start + len] && (start + len) < BUFFER_SIZE)
 		len++;
 	index = 0;
 	while (index < len)
 	{
-		buffer[index] = buffer[start + index];
+		rests[fd][index] = rests[fd][start + index];
 		index++;
 	}
-	buffer[index] = '\0';
+	rests[fd][index] = '\0';
 }
 
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1];
+	static char	rests[1025][BUFFER_SIZE + 1];
 	char		*line;
 
 	if (fd > 1024 || fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	line = load_line(fd, buffer, NULL);
+	line = load_line(fd, rests, NULL);
 	if (line)
-		save_rest(buffer);
+		save_rest(fd, rests);
 	return (line);
 }
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,11 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gabach <gabach@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/19 15:53:21 by gabach            #+#    #+#             */
-/*   Updated: 2025/11/27 11:19:41 by gabach           ###   ########lyon.fr   */
+/*   Created: 2025/11/18 15:19:37 by gabach            #+#    #+#             */
+/*   Updated: 2025/11/27 11:17:36 by gabach           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+// INCLUDES
+
+# include <stdlib.h>
+# include <unistd.h>
+
+// MACROS
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+// FUNCTIONS
+
+char	*get_next_line(int fd);
+
+#endif
